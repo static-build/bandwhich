034661f0115c5f5b0072a2d327a867adc3b754cd
edwinjhlee <edwin.jh.lee@gmail.com>
2021-07-21 16:50:49 +0800

    ...

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 
+
 prepare(){
     if [ -d code ]; then
         echo "Already exists"
-        git subtree pull --prefix code https://github.com/imsnif/bandwhich.git main --squash
+        git subtree pull --prefix code https://github.com/imsnif/bandwhich.git 0.20.0 --squash
     else
-        git subtree add --prefix code https://github.com/imsnif/bandwhich.git main --squash
+        git subtree add --prefix code https://github.com/imsnif/bandwhich.git 0.20.0 --squash
     fi
 }
+
+appname=bandwidth
 
 # apt-get install mingw-w64
 # apt install gcc
@@ -16,12 +19,12 @@ build_win(){
         cd code
         cross build --target x86_64-pc-windows-gnu --release
     )
-    cp code/target/x86_64-pc-windows-gnu/release/dust.exe bin/dust.x64.exe
+    cp code/target/x86_64-pc-windows-gnu/release/${appname}.exe bin/${appname}.x64.exe
     (
         cd bin
         # xrc 7z
-        # 7z a dust.x64.exe.7z dust.x64.exe
-        # rm dust.x64.exe
+        # 7z a ${appname}.x64.exe.7z ${appname}.x64.exe
+        # rm ${appname}.x64.exe
     )
 }
 
@@ -29,7 +32,7 @@ build_main(){
 
     local rust_target=${1:?rust_target}
     local target_name=${2:?binary}
-    local exe=${3:-dust}
+    local exe=${3:-${appname}}
 
     echo "Building $rust_target with $target_name"
 
@@ -51,19 +54,19 @@ build_main(){
 }
 
 build_linux_x64(){
-    build_main x86_64-unknown-linux-musl dust.linux.x64
+    build_main x86_64-unknown-linux-musl ${appname}.linux.x64
 }
 
 build_linux_arm64(){
-    build_main aarch64-unknown-linux-musl dust.linux.arm64
+    build_main aarch64-unknown-linux-musl ${appname}.linux.arm64
 }
 
 build_linux_armv7hf(){
-    build_main armv7-unknown-linux-musleabihf dust.linux.armv7hf
+    build_main armv7-unknown-linux-musleabihf ${appname}.linux.armv7hf
 }
 
 build_linux_armv7(){
-    build_main armv7-unknown-linux-musleabi dust.linux.armv7
+    build_main armv7-unknown-linux-musleabi ${appname}.linux.armv7
 }
 
 main(){
@@ -72,8 +75,8 @@ main(){
     # build_linux_armv7
     build_linux_armv7hf
 
-    build_main aarch64-apple-darwin dust.darwin.arm64
-    build_main x86_64-apple-darwin dust.darwin.x64
+    build_main aarch64-apple-darwin ${appname}.darwin.arm64
+    build_main x86_64-apple-darwin ${appname}.darwin.x64
 
-    build_main x86_64-pc-windows-gnu dust.x64.exe dust.exe
+    build_main x86_64-pc-windows-gnu ${appname}.x64.exe ${appname}.exe
 }
