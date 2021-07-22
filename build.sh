0ee84859e09e56e2e38505f4eeadf1a2a3beda53
edwinjhlee <edwin.jh.lee@gmail.com>
2021-07-22 08:37:37 +0800

    ...

--- build.sh
+++ build.sh
@@ -23,7 +23,8 @@ build_win(){
     (
         cd bin
         xrc 7z
-        _7z a ${appname}.x64.exe.7z ${appname}.x64.exe
+        _7z a -v1000k ${appname}.x64.exe.7z ${appname}.x64.exe
+        ls ${appname}.x64.exe.7z* | wc -l | tr -cd '0-9' > ${appname}.x64.exe.7z
         rm ${appname}.x64.exe
     )
 }
@@ -37,6 +38,7 @@ build_main(){
     echo "Building $rust_target with $target_name"
 
     [ -f "bin/$target_name.7z" ] && return 0
+    [ -f "bin/$target_name.7z.001" ] && return 0
 
     (
         cd code && {
@@ -46,34 +48,23 @@ build_main(){
         cp "code/target/$rust_target/release/$exe" "bin/$target_name" && (
             cd bin && {
                 xrc 7z
-                _7z a "$target_name.7z" "$target_name"
+                _7z -v1000k a "$target_name.7z" "$target_name"
+                ls $target_name.7z* | wc -l | tr -cd '0-9' > "$target_name.7z"
                 rm "$target_name"
             }
         )
     }
 }
 
-build_linux_x64(){
-    build_main x86_64-unknown-linux-musl ${appname}.linux.x64
-}
-
-build_linux_arm64(){
-    build_main aarch64-unknown-linux-musl ${appname}.linux.arm64
-}
-
-build_linux_armv7hf(){
-    build_main armv7-unknown-linux-musleabihf ${appname}.linux.armv7hf
-}
-
-build_linux_armv7(){
-    build_main armv7-unknown-linux-musleabi ${appname}.linux.armv7
-}
 
 main(){
-    build_linux_x64
-    build_linux_arm64
-    # build_linux_armv7
-    build_linux_armv7hf
+    build_main x86_64-unknown-linux-musl ${appname}.linux.x64
+
+    build_main aarch64-unknown-linux-musl ${appname}.linux.arm64
+    # build_main aarch64-unknown-linux-gnu ${appname}.linux.arm64
+
+    # build_main armv7-unknown-linux-musleabi ${appname}.linux.armv7
+    build_main armv7-unknown-linux-musleabihf ${appname}.linux.armv7hf
 
     build_main aarch64-apple-darwin ${appname}.darwin.arm64
     build_main x86_64-apple-darwin ${appname}.darwin.x64
